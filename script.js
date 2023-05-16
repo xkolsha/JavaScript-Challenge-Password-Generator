011c692686baee226f5204e69c05b5c00bb0547f
xkolsha <aviad85@outlook.com>
2023-05-16 19:11:50 -0300

    removed { that broke code+add for loop and return

--- script.js
+++ script.js
@@ -18,31 +18,37 @@ function generatePassword() {
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var symbols = "!@#$%^&*()-=_+[]{}|;:,.<>?";
-}
 
-var length = parseInt(
-  prompt("Choose a password length between 8 and 128 characters:")
-);
-if (isNaN(length) || length < 8 || length > 128) {
-  alert("Invalid input. Please choose a length between 8 and 128.");
-  return "";
-}
+  var length = parseInt(
+    prompt("Choose a password length between 8 and 128 characters:")
+  );
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid input. Please choose a length between 8 and 128.");
+    return "";
+  }
 
-var charset = "";
-if (confirm("Include lowercase characters?")) {
-  charset += lowercase;
-}
-if (confirm("Include uppercase characters?")) {
-  charset += uppercase;
-}
-if (confirm("Include numeric characters?")) {
-  charset += numbers;
-}
-if (confirm("Include special characters?")) {
-  charset += symbols;
-}
+  var charset = "";
+  if (confirm("Include lowercase characters?")) {
+    charset += lowercase;
+  }
+  if (confirm("Include uppercase characters?")) {
+    charset += uppercase;
+  }
+  if (confirm("Include numeric characters?")) {
+    charset += numbers;
+  }
+  if (confirm("Include special characters?")) {
+    charset += symbols;
+  }
 
-if (charset === "") {
-  alert("You need to select at least one type of characters to include.");
-  return "";
+  if (charset === "") {
+    alert("You need to select at least one type of characters to include.");
+    return "";
+  }
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += charset[Math.floor(Math.random() * charset.length)];
+  }
+
+  return password;
 }
